cbcb1a3708a4f26d4afac4f35d69eb6137acda81
dante-signal31 <dante.signal31@gmail.com>
2017-04-08 00:10:58 +0200

    Version update.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vdist',
-    version='1.0',
+    version='1.1',
     description='Create OS packages from Python '
                 'projects using Docker containers',
     long_description='Create OS packages from Python '
